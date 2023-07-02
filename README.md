9fae7bc3c5e134833e36769f4e26ed20a659442d
Konstantin Perikov <pericov.ko@gmail.com>
2023-07-02 17:27:32 +0100

    feat: adding initla version of the program

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# lingua-gap-fill
+# LinguaGapFill
+
 LinguaGapFill is an innovative linguistic study tool aimed at examining and improving the process of language acquisition. It utilizes a method similar to the Masked Language Model approach, where participants are required to fill in missing words in sentences, providing a unique insight into how humans learn new languages
+
+### Need to support auto-generate of questionnaire from Excel/CSV spreadsheet with timing of the answers
+
+### How to run
+
+```
+npm install --save-dev electron
+npm install --save-dev electron-winstaller
+npm install electron-packager
+npm start
+```
+
+### How to package app
+
+```
+electron-packager . --platform=win32 --arch=x64
+```
+
+### How to create installer
+
+```
+node installers/windows/createinstaller.js
+```
+
+### How to support linters
+
+```
+npm install eslint --save-dev
+npm init @eslint/config
+npm install --save-dev --save-exact prettier
+npm install --save-dev eslint-config-prettier eslint-plugin-prettier
+```
+
+add the following lines to `package.json` under scripts section
+
+```
+"lint": "eslint .",
+"format": "prettier --write ."
+```
+
+Use the following commands:
+
+`npm run lint` or `npm run format`
---
+++ renderer.js
@@ -0,0 +1,106 @@
+const fs = require("fs");
+const nextPageBtn = document.querySelector("#nextPage");
+const questionText = document.querySelector("#questionText");
+
+let currentQuestionId = 0;
+let startTime = 0;
+
+const questions = [
+  "Мегрэ снял тонкую кожуру _________ ножом и впился зубами в сочную бело-голубую мякоть.",
+  "Держа бугристый _____________ в ладонях, Амелия думала: «Интересно, каков на вкус этот зеленый фрукт?»",
+  "Чтобы попробовать красноватую мякоть, мне сначала пришлось расцеплять переплетенные стручки ____________.",
+  "Издалека мальчику казалось, что дерево покрыто цветами, но, подойдя ближе, он с удивлением обнаружил плоды _________ с тремя крупными черными семенами.",
+  "__________ был настолько спелым, что у некоторых плодов чешуйчатая кожура полопалась, обнажив белую мякоть, похожую на фундук.",
+  "Мякоть гладкого зеленого __________ богата витаминами класса В.",
+  "Счистить кожуру _____________ оказалось не так-то просто из-за мелких колючек, зато вязкие семена легко вычищались ложкой.",
+  "Бурый ____________ приглянулся детям: они выдавливали мякоть плода в пиалы, и под веселый смех предлагали ее гостям в качестве икры.",
+  "Спелые гладкие ___________, лежащие на земле, были похожи на потрескавшиеся красно-оранжевые каштаны, только без колючек.",
+  "Катарина нарезала ____________ так, чтобы не повредить бело-голубую мякоть, а мелкие черные косточки постаралась удалить.",
+  "Глянцевые чешуйки на тонкой кожуре __________ наводили Джона на мысль о том, что ее можно было бы использовать для создания каких-нибудь аксессуаров.",
+  "Спелый ____________  содержит 6-10 сегментов в красно-зеленом стручке.",
+  "Среди зеленых фруктов, лежащих в корзине, Питер сразу узнал __________ по его гладкой кожуре.",
+  "Считается, что сок бугристого зеленого ____________ полезен для здоровья и продлевает жизнь.",
+  "Донна Роза убирала грядки с оранжевыми плодами ____________ в перчатках, поскольку они в этом году были сильно колючими.",
+  "Том хотел приготовить огуречный салат из подаренного вождем племени гладкого вытянутого ___________, но вид мякоти привел его в замешательство. ",
+  "Фиолетовый продолговатый ____________ показался Марии таким необычным, что ей захотелось вырастить его у себя на балконе.",
+  "Спелые красно-оранжевые плоды ___________ лучше собирать пока они не раскрылись, чтобы не приходилось отмачивать чёрные косточки в кипятке.",
+  "Закрученная в кольцо форма бархатистого ______________ позволяет хранить его, развесив на веревке.",
+  "Если бы не крупные темные семена, зеленый ___________ можно было бы принять за ананас.",
+  "Коричневый и шершавый ______________ сложно разглядеть среди веток дерева, на котором он растет. ",
+  "Чтобы добраться до сочной желтой мякоти ______________, нужно удалить крупные красные семена.",
+  "Розита так аккуратно собирала гладкие, зеленые плоды __________, что ей удавалось даже сохранить шляпки.",
+  "Ловким движением надрезая тонкую кожуру _____________, Адам выпускал зернистую мякоть в миску.",
+];
+
+function getResults() {
+  const fileName = "results.log";
+  const checked = Array.from(
+    document.querySelectorAll("input:checked"),
+    (radio) => radio.id
+  );
+  const time = Date.now() - startTime;
+
+  console.log(checked);
+  console.log(time);
+
+  startTime = Date.now();
+  fs.appendFileSync(fileName, `${checked.join(",")},${time}\n`);
+}
+
+function uncheckAllRadios() {
+  const radioButtons = document.querySelectorAll("input[type=radio]");
+  radioButtons.forEach((radioButton) => (radioButton.checked = false));
+}
+
+nextPageBtn.onclick = () => {
+  if (nextPageBtn.innerText === "Завершить тест") {
+    window.close();
+  }
+
+  const allClicked =
+    Array.from(document.querySelectorAll('input[name="answer"]:checked'))
+      .length === 1;
+
+  if (currentQuestionId === 24) {
+    getResults();
+    console.log("no more questions to ask");
+    document.querySelector("#questionnaire").hidden = true;
+    questionText.innerText = "Спасибо за выполнение задания!";
+    nextPageBtn.innerText = "Завершить тест";
+  } else if (currentQuestionId === 0) {
+    startTime = Date.now();
+
+    questionText.innerText = questions[currentQuestionId];
+    document.querySelector("#options-list").hidden = false;
+    document.querySelector("#instruction").hidden = true;
+    nextPageBtn.innerText = "Далее";
+
+    currentQuestionId++;
+  } else {
+    const answer1 = document.querySelector('input[name="answer"]:checked');
+    const answer2 = document.querySelector('input[name="q1"]:checked');
+
+    if (answer1) {
+      getResults();
+      document.querySelector("#options-list").hidden = true;
+      document.querySelector("#certaintyQuestionText").hidden = false;
+      questionText.hidden = true;
+      document.querySelector("#questionnaire").hidden = false;
+
+      uncheckAllRadios();
+      console.log("clicked1");
+    } else if (answer2) {
+      getResults();
+      document.querySelector("#options-list").hidden = false;
+      document.querySelector("#certaintyQuestionText").hidden = true;
+      questionText.hidden = false;
+      document.querySelector("#questionnaire").hidden = true;
+
+      questionText.innerText = questions[currentQuestionId];
+      currentQuestionId++;
+
+      uncheckAllRadios();
+      console.log("clicked2");
+    }
+  }
+};
